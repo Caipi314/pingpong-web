1b675492223af3a96a21fcf605b3c7ec7a788c97
Caipi314 <caipsiegel@gmail.com>
2021-02-22 12:14:30 -0500

    port

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '\\public\\index.html'));
 })
 
-app.listen((port = 3000), () => {
+app.listen((port = 3000 || process.env.PORT), () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 })
