dfa1030a126ebd8cb9d28339b85be18fe54160ff
Caipi314 <caipsiegel@gmail.com>
2021-02-22 15:51:13 -0500

    nother test

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 
 // console.log('object')
 app.get('/', (req, res) => {
+	// res.sendFile(path.join(__dirname + '\\public\\index.html'));
 	res.sendFile(path.join(__dirname + '\\public\\index.html'));
 })
 app.get('/game', (req, res) => {
-	res.sendFile(path.join(__dirname + '\\public\\game.html'));
+	// res.sendFile(path.join(__dirname + '\\public\\game.html'));
+	res.sendFile(path.join(__dirname + '/public/game.html'));
 })
 app.post('/createGame', (req, res) => {
 	const p1 = req.body.p1;
