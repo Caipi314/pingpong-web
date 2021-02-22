9f0de67d029ef8386bf45a70286334c36dcf5d17
Caipi314 <caipsiegel@gmail.com>
2021-02-22 15:41:01 -0500

    more

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const fs = require('fs');
+// const { body, validationResult } = require('express-validator');
 const express = require('express')
 const app = express();
 const path = require('path');
-app.use(express.static('public'))
 
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+
+// console.log('object')
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '\\public\\index.html'));
 })
+app.get('/game', (req, res) => {
+	res.sendFile(path.join(__dirname + '\\public\\game.html'));
+})
+app.post('/createGame', (req, res) => {
+	const p1 = req.body.p1;
+	const p2 = req.body.p2;
+	const ptsToWin = req.body.ptsToWin;
+	const servesPerPlayer = req.body.servesPerPlayer;
+	const bestOf = req.body.bestOf;
+	// console.log({ p1, p2, ptsToWin, servesPerPlayer, bestOf })
+	res.redirect(`/game/?P1=${p1}&P2=${p2}&PTW=${ptsToWin}&SP=${servesPerPlayer}&BOf=${bestOf}`);
+})
 
-app.listen((port = process.env.PORT || 3000), () => {
+app.listen((port = (process.env.PORT || 3000)), () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 })
--- public/JS/game.js
+++ public/JS/game.js
@@ -2,15 +2,16 @@ const pointCounters = document.querySelectorAll('div.points');
 const serveBalls = document.querySelectorAll('svg');
 const scorecards = document.querySelectorAll('div.scorecard');
 const gamesPer = document.querySelectorAll('span.gamesWon');
+const names = document.querySelectorAll('div.scorecard span.name');
 const winnerBanner = document.querySelector('#winner');
 const gameNumber = document.querySelector('span#gameNumber');
 const time = document.querySelector('div#time');
-//todo time clock
 class Game {
-	constructor(bestOf, servesPer, pointsToWin) {
-		this.bestOf = bestOf;
-		this.servesPer = servesPer;
+	constructor(pNames, pointsToWin, servesPer, bestOf) {
+		this.pNames = pNames;
 		this.ptsToWin = pointsToWin;
+		this.servesPer = servesPer;
+		this.bestOf = bestOf;
 		this.winBy = 2;
 		this.gameStart = new Date();
 
@@ -18,14 +19,20 @@ class Game {
 		this.points = [0, 0];
 
 		this.server = null;
-		pointCounters.forEach(el => el.textContent = '0');
+		pointCounters.forEach(el => el.textContent = 0);
 		setInterval(this.updateTime, 200, this.gameStart);
+		names.forEach((el, pNum) => el.textContent = this.pNames[pNum]);
 	}
 	updateTime(date) {
 		const secElaps = Math.floor((new Date() - date) / 1000);
 		const minutes = Math.floor(secElaps / 60) % 60;
 		const seconds = secElaps % 60;
 		time.textContent = `${minutes}:${seconds > 9 ? '' : '0'}${seconds}`;
+	}
+	set banner(text) {
+		winnerBanner.textContent = text;
+		winnerBanner.classList.remove('hidden');
+		setTimeout(() => winnerBanner.classList.add('hidden'), 4000);
 	}
 	set server(pNum) {
 		if (pNum == null) {
@@ -40,43 +47,44 @@ class Game {
 		else if (!serveBalls[1].classList.contains('hidden')) { return 1 }
 		else { return null }
 	}
-	matchFor(pNum) {
-		winnerBanner.textContent = `Player ${pNum + 1} Has Won the Match!`;
-		winnerBanner.classList.remove('hidden');
-		setTimeout(() => winnerBanner.classList.add('hidden'), 5000);
-
-
+	get totalPoints() {
+		return this.points[0] + this.points[1];
+	}
+	get totalGames() {
+		return this.games[0] + this.games[1];
 	}
 	gameFor(pNum) {
-		winnerBanner.textContent = `Player ${pNum + 1} Wins!`;
-		winnerBanner.classList.remove('hidden');
-		setTimeout(() => winnerBanner.classList.add('hidden'), 4000);
+		//if won the match
+		if (this.games[pNum] == Math.floor(this.bestOf / 2)) {
+			this.banner = `${this.pNames[pNum]} Wins The Match in ${this.totalGames + 1} Games!`;
+			setTimeout(() => document.location.href = '/', 4000);
+			return;
+		}
+		// they only won the game
+		this.banner = `${this.pNames[pNum]} Wins Game #${this.totalGames + 1}!`;
 
 		// reset scores and serve
 		this.points = [0, 0];
 		pointCounters.forEach(el => el.textContent = 0);
 		this.server = null;
 
-		// increase local and client values of games (+2 for return value's offset)
+		// update games per
 		gamesPer[pNum].textContent = this.games[pNum]++ + 1;
+		// update game number
+		gameNumber.textContent = this.totalGames + 1;
 
-		gameNumber.textContent = this.games[0] + this.games[1] + 1;
 
-		//if won the match
-		if (this.games.some(gamesWon => gamesWon == Math.ceil(this.bestOf / 2))) {
-			matchFor(this.games.indexOf(Math.max(...this.games)));
-		}
 	}
 	pointFor(pNum) {
-		// console.log(this.server)
 		//	rally for serve
 		if (this.points.every(pPts => pPts == 0) && this.server === null) {
-			return this.server = pNum;
+			this.server = pNum;
+			return;
 		}
 		// increase local and client values of points (+1 for return value's offset)
 		pointCounters[pNum].textContent = this.points[pNum]++ + 1;
 		// switch serve
-		if ((this.points[0] + this.points[1]) % this.servesPer == 0) {
+		if (this.totalPames % this.servesPer == 0) {
 			this.server = !this.server | 0;
 		}
 		//win by 2
@@ -85,16 +93,23 @@ class Game {
 		}
 		//deadmans
 		if (this.points.some(pPts => pPts + 1 == this.ptsToWin)) {
-			this.server = this.points.indexOf(Math.min(...this.points));
+			this.server = !pNum | 0;
 		}
 		// winner
-		if (this.points.some(pPts => pPts == this.ptsToWin)) {
-			this.gameFor(this.points.indexOf(Math.max(...this.points)));
+		if (this.points[pNum] == this.ptsToWin) {
+			this.gameFor(pNum);
 		}
 	}
 }
-const game = new Game(3, 2, 11);
-
+const url_string = window.location.href;
+const url = new URL(url_string);
+const p1Name = url.searchParams.get("P1");
+const p2Name = url.searchParams.get("P2");
+const pointsToWin = parseInt(url.searchParams.get("PTW"));
+const servesPer = parseInt(url.searchParams.get("SP"));
+const bestOf = parseInt(url.searchParams.get("BOf"));
+// ?P1=cai&P2=test&PTW=11&SP=2&BOf=3
+const game = new Game([p1Name, p2Name], pointsToWin, servesPer, bestOf);
 scorecards.forEach((el, pNum) => {
 	el.onclick = () => {
 		game.pointFor(pNum);
